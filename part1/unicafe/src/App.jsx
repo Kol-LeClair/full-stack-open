280b7fbe65211289d292c10012f016040b36826c
Kol-LeClair <leclair.kol@icloud.com>
2025-12-03 18:52:13 -0500

    Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -20,6 +20,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {good + neutral + bad}</div>
+      <div>average {(good + (bad * - 1)) / (good + bad + neutral)}</div>
+      <div>positive {(good / (good + neutral + bad)) * 100} %</div>
     </div>
   )
 }

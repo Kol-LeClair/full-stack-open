6f09800bd67cc785a6fa700f3862f30e3072b352
Kol-LeClair <leclair.kol@icloud.com>
2025-12-06 23:35:26 -0500

    Exercises 2.1, 2.2, 2.3

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,57 @@
+const Header = (props) => {
+  return <h1>{props.course.name}</h1>
+}
+
+const Part = (props) => {
+  return <p>{props.part} {props.exercises}</p>
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises} />)}
+    </div>
+  )
+}
+
+const Total = ({ course }) => {
+  return <p>Number of exercises {course.parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App

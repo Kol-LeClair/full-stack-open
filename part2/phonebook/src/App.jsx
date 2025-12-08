039d6ae6f0590775fa16cc2247a7095b9d9c2dcf
Kol-LeClair <leclair.kol@icloud.com>
2025-12-08 11:51:19 -0500

    Exercises 2.16 & 2.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,22 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+import './index.css'
+
 import personService from './services/persons'
 
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+  const [message, setMessage] = useState(null)
+  const [isError, setIsError] = useState(false)
 
   useEffect(() => {
     personService
@@ -43,6 +48,20 @@ const App = () => {
             setPersons(persons.map(person => person.name === newName ? returnedPerson : person))
             setNewName('')
             setNewNumber('')
+
+            setMessage(`Updated ${returnedPerson.name}'s number to ${returnedPerson.number}`)
+            setTimeout(() => {
+            setMessage(null)
+          }, 3000)
+          })
+          .catch(error => {
+            setIsError(true)
+            setMessage(`Information of ${person.name} has already been removed from server`)
+            setPersons(persons.filter(person => person.name != newName))
+            setTimeout(() => {
+              setMessage(null)
+              setIsError(false)
+            }, 3000)
           })
       }
     } else {   
@@ -52,6 +71,11 @@ const App = () => {
           setPersons(persons.concat(returnedPerson))
           setNewName('')
           setNewNumber('')
+
+          setMessage(`Added ${returnedPerson.name}`)
+          setTimeout(() => {
+            setMessage(null)
+          }, 3000)
         })
     }
   }
@@ -73,6 +97,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} isError={isError} />
       <Filter handleSearchChange={handleSearchChange} />
       <h3>add a new</h3>
       <PersonForm 

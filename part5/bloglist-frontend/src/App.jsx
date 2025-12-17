3b471efa2136ef29ea931e4700a12af1c168a867
Kol-LeClair <leclair.kol@icloud.com>
2025-12-17 13:00:23 -0500

    Exercises 5.2 & 5.3

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import BlogForm from './components/BlogForm'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -8,6 +9,9 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -15,11 +19,26 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
     try {
       const user = await loginService.login({ username, password })
+      
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -32,6 +51,26 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
       */
+    }
+  }
+
+  const handleBlog = async (event) => {
+    event.preventDefault()
+    try {
+      const blogObject = {
+        title: title,
+        author: author,
+        url: url
+      }
+
+      const returnedBlog = await blogService.create(blogObject)
+
+      setBlogs(blogs.concat(returnedBlog))
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    } catch {
+      console.log('error')
     }
   }
 
@@ -70,10 +109,30 @@ const App = () => {
       {user && (
         <div>
           <h2>blogs</h2>
-          <p>{user.name} logged in</p>
+
+          <p>{user.name} logged in
+            <button onClick={() => {
+              window.localStorage.removeItem('loggedBlogappUser')
+              setUser(null)
+            }}>
+              logout
+            </button>
+          </p>
+
+          <BlogForm 
+            handleBlog={handleBlog} 
+            title={title} 
+            author={author} 
+            url={url}
+            setTitle={setTitle}
+            setAuthor={setAuthor}
+            setUrl={setUrl}
+          />
+
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
+
         </div>
       )}
 

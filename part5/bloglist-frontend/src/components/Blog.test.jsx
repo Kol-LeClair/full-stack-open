14696daa9eb031a723cc7d12d33ec8a5c42d035a
Kol-LeClair <leclair.kol@icloud.com>
2025-12-19 11:01:59 -0500

    Exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title and author, but does not automatically show url or likes', () => {
@@ -17,4 +18,31 @@ test('renders title and author, but does not automatically show url or likes', (
 
     expect(screen.queryByText('Url.com')).toBeNull()
     expect(screen.queryByText('likes')).toBeNull()
+
+    // screen.debug()
+    // screen.debug(element)
+})
+
+test('clicking view button shows url and likes', async () => {
+    const blog = {
+        title: 'Title',
+        author: 'Author',
+        url: 'Url.com',
+        user: {
+            name: 'Me'
+        }
+    }
+
+    // const mockHandler = vi.fn()
+
+    render( <Blog blog={blog} user={blog.user} />)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const element = screen.getByText('Url.com')
+    expect(element).toBeVisible()
+
+    // expect(mockHandler.mock.calls).toHaveLenght(1)
 })

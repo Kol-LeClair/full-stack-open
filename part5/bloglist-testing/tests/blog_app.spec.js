70a0f4a3dab9ebcdca31364914c7105a845ce037
Kol-LeClair <leclair.kol@icloud.com>
2025-12-19 15:17:28 -0500

    Exercise 5.17

---
+++ part5/bloglist-testing/tests/blog_app.spec.js
@@ -0,0 +1,13 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    const locator = page.getByText('username')
+    await expect(locator).toBeVisible()
+    await expect(page.getByText('log in to application')).toBeVisible()
+  })
+})

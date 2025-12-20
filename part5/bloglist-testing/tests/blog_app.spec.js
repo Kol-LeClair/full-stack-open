02488f9db730f89535ac76b35c880c7c4a6d0530
Kol-LeClair <leclair.kol@icloud.com>
2025-12-20 10:34:12 -0500

    Exercise 5.18

--- part5/bloglist-testing/tests/blog_app.spec.js
+++ part5/bloglist-testing/tests/blog_app.spec.js
@@ -1,7 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+        data: {
+            name: 'Penelope',
+            username: 'username88',
+            password: 'password88'
+        }
+    })
+    
     await page.goto('http://localhost:5173')
   })
 
@@ -9,5 +18,23 @@ describe('Blog app', () => {
     const locator = page.getByText('username')
     await expect(locator).toBeVisible()
     await expect(page.getByText('log in to application')).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByRole('textbox').first().fill('username88')
+      await page.getByRole('textbox').last().fill('password88')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('Penelope logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByRole('textbox').first().fill('username')
+      await page.getByRole('textbox').last().fill('password')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('log in to application')).toBeVisible()
+    })
   })
 })
